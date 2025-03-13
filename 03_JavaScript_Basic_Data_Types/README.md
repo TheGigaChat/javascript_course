9e3c46f56217302a588cc4f7452c286b6cd6ebe1
Artur Dzekunov <151076331+TheGigaChat@users.noreply.github.com>
2025-03-13 17:51:50 +0200

    03 Lesson Files

---
+++ 03_JavaScript_Basic_Data_Types/README.md
@@ -0,0 +1,31 @@
+
+## Video link
+
+[JavaScript Data Types for Beginners](https://www.youtube.com/watch?v=1rzFdUFVAxU)
+
+
+## Steps from the Video
+ - Numbers
+ - Strings
+ - Undefined
+ - Boolean
+ - Code Challenge
+
+
+
+
+
+## Help
+
+ - [How to Install Visual Studio Code](https://www.youtube.com/watch?v=wyxkkelEdLM&t=1s)
+ - [How to Download GitHub Files and Run It](https://www.youtube.com/watch?v=iWEWje-c_t0)
+
+
+## Authors
+
+Codeimas
+- [@JavaScript Guide](https://www.youtube.com/watch?v=bdr3fMwEDks)
+
+Code Creator
+- [@Codeimas](https://github.com/TheGigaChat)
+
---
+++ 03_JavaScript_Basic_Data_Types/script.js
@@ -0,0 +1,64 @@
+// Recommended rules. I will explain one part, then show you the image, you write the code to your PC
+// At the end, you will have a code challenge
+
+
+// Numbers
+50 - 20
+console.log(50 - 20)
+
+let num1 = 10 - 2
+console.log(num1)
+num1 = num1 + 92
+console.log(num1)
+num2 = num1
+console.log(num2)
+
+num1 = num1 - num1
+console.log(num1)
+
+
+// Strings
+'Zebra'  // you need to use quotes
+let animal = 'Zebra'
+console.log(animal)
+let myPet = animal // camelCase
+console.log(myPet)
+myPet = 'animal'
+console.log(myPet)
+"Zebra", 'Zebra' // the same thing
+console.log("Zebra", 'Zebra')
+
+
+// Boolean
+true, false  // lower letter only
+let isLight = true
+console.log(isLight)
+isLight = false
+console.log(isLight)
+
+
+// Undefined
+null, undefined
+let cell
+console.log(cell)
+
+
+// Code challenge
+let isLightning
+console.log(isLightning)
+
+let energy = 0
+let energyAmount = 5000
+isLightning = false
+console.log(isLightning, energy, energyAmount)
+
+isLightning = true
+energy = energy + energyAmount
+console.log(isLightning, energy)
+
+isLightning = false
+console.log(isLightning, energy)
+
+isLightning = true
+energy = energy + energyAmount
+console.log(isLightning, energy)
