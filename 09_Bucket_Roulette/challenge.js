006b677359f86e262d7d127bbeadfd232accd8d4
Artur Tudeng <ardzek@taltech.ee>
2025-04-17 19:18:04 +0300

    Simplify the project for beginners. Change the winners check and the default outcome of the game.

--- 09_Bucket_Roulette/challenge.js
+++ 09_Bucket_Roulette/challenge.js
@@ -3,7 +3,7 @@
 let playerLives = 2
 let dealerLives = 2
 let bullet
-let playerChoice
+let playerChoice  // choose the target
 
 // First cycle
 if (playerLives > 0 && dealerLives > 0) {
@@ -12,16 +12,16 @@ if (playerLives > 0 && dealerLives > 0) {
 
     if (bullet === 1 && playerChoice) {
         dealerLives = dealerLives - 1
-        console.log("😃 You hit the dealer! Dealer loses a life. Remaining lives: " + (dealerLives))
+        console.log("😃 You hit the dealer! Dealer loses a life. Remaining lives: " + dealerLives)
     } else if (bullet === 1 && !playerChoice) {
         playerLives = playerLives - 1
-        console.log("😢 Oops! You shot yourself! You lose a life. Remaining lives: " + (playerLives))
+        console.log("😢 Oops! You shot yourself! You lose a life. Remaining lives: " + playerLives)
     } else if (bullet === 0 && playerChoice) {
         playerLives = playerLives - 1
-        console.log("😢 Empty shot! Dealer counterattacks! You lose a life. Remaining lives: " + (playerLives))
+        console.log("😢 Empty shot! Dealer counterattacks! You lose a life. Remaining lives: " + playerLives)
     } else {
         playerLives = playerLives + 1
-        console.log("💖 Lucky break! You gain an extra life. Total lives: " + (playerLives))
+        console.log("💖 Lucky break! You gain an extra life. Total lives: " + playerLives)
     }
 
 } // End of First cycle
@@ -33,16 +33,16 @@ if (playerLives > 0 && dealerLives > 0) {
 
     if (bullet === 1 && playerChoice) {
         dealerLives = dealerLives - 1
-        console.log("😃 You hit the dealer! Dealer loses a life. Remaining lives: " + (dealerLives))
+        console.log("😃 You hit the dealer! Dealer loses a life. Remaining lives: " + dealerLives)
     } else if (bullet === 1 && !playerChoice) {
         playerLives = playerLives - 1
-        console.log("😢 Oops! You shot yourself! You lose a life. Remaining lives: " + (playerLives))
+        console.log("😢 Oops! You shot yourself! You lose a life. Remaining lives: " + playerLives)
     } else if (bullet === 0 && playerChoice) {
         playerLives = playerLives - 1
-        console.log("😢 Empty shot! Dealer counterattacks! You lose a life. Remaining lives: " + (playerLives))
+        console.log("😢 Empty shot! Dealer counterattacks! You lose a life. Remaining lives: " + playerLives)
     } else {
         playerLives = playerLives + 1
-        console.log("💖 Lucky break! You gain an extra life. Total lives: " + (playerLives))
+        console.log("💖 Lucky break! You gain an extra life. Total lives: " + playerLives)
     }
 
 } // End of Second cycle
@@ -54,16 +54,16 @@ if (playerLives > 0 && dealerLives > 0) {
 
     if (bullet === 1 && playerChoice) {
         dealerLives = dealerLives - 1
-        console.log("😃 You hit the dealer! Dealer loses a life. Remaining lives: " + (dealerLives))
+        console.log("😃 You hit the dealer! Dealer loses a life. Remaining lives: " + dealerLives)
     } else if (bullet === 1 && !playerChoice) {
         playerLives = playerLives - 1
-        console.log("😢 Oops! You shot yourself! You lose a life. Remaining lives: " + (playerLives))
+        console.log("😢 Oops! You shot yourself! You lose a life. Remaining lives: " + playerLives)
     } else if (bullet === 0 && playerChoice) {
         playerLives = playerLives - 1
-        console.log("😢 Empty shot! Dealer counterattacks! You lose a life. Remaining lives: " + (playerLives))
+        console.log("😢 Empty shot! Dealer counterattacks! You lose a life. Remaining lives: " + playerLives)
     } else {
         playerLives = playerLives + 1
-        console.log("💖 Lucky break! You gain an extra life. Total lives: " + (playerLives))
+        console.log("💖 Lucky break! You gain an extra life. Total lives: " + playerLives)
     }
 
 } // End of Third cycle
@@ -75,16 +75,16 @@ if (playerLives > 0 && dealerLives > 0) {
 
     if (bullet === 1 && playerChoice) {
         dealerLives = dealerLives - 1
-        console.log("😃 You hit the dealer! Dealer loses a life. Remaining lives: " + (dealerLives))
+        console.log("😃 You hit the dealer! Dealer loses a life. Remaining lives: " + dealerLives)
     } else if (bullet === 1 && !playerChoice) {
         playerLives = playerLives - 1
-        console.log("😢 Oops! You shot yourself! You lose a life. Remaining lives: " + (playerLives))
+        console.log("😢 Oops! You shot yourself! You lose a life. Remaining lives: " + playerLives)
     } else if (bullet === 0 && playerChoice) {
         playerLives = playerLives - 1
-        console.log("😢 Empty shot! Dealer counterattacks! You lose a life. Remaining lives: " + (playerLives))
+        console.log("😢 Empty shot! Dealer counterattacks! You lose a life. Remaining lives: " + playerLives)
     } else {
         playerLives = playerLives + 1
-        console.log("💖 Lucky break! You gain an extra life. Total lives: " + (playerLives))
+        console.log("💖 Lucky break! You gain an extra life. Total lives: " + playerLives)
     }
 
 } // End of Fourth cycle
@@ -96,27 +96,30 @@ if (playerLives > 0 && dealerLives > 0) {
 
     if (bullet === 1 && playerChoice) {
         dealerLives = dealerLives - 1
-        console.log("😃 You hit the dealer! Dealer loses a life. Remaining lives: " + (dealerLives))
+        console.log("😃 You hit the dealer! Dealer loses a life. Remaining lives: " + dealerLives)
     } else if (bullet === 1 && !playerChoice) {
         playerLives = playerLives - 1
-        console.log("😢 Oops! You shot yourself! You lose a life. Remaining lives: " + (playerLives))
+        console.log("😢 Oops! You shot yourself! You lose a life. Remaining lives: " + playerLives)
     } else if (bullet === 0 && playerChoice) {
         playerLives = playerLives - 1
-        console.log("😢 Empty shot! Dealer counterattacks! You lose a life. Remaining lives: " + (playerLives))
+        console.log("😢 Empty shot! Dealer counterattacks! You lose a life. Remaining lives: " + playerLives)
     } else {
         playerLives = playerLives + 1
-        console.log("💖 Lucky break! You gain an extra life. Total lives: " + (playerLives))
+        console.log("💖 Lucky break! You gain an extra life. Total lives: " + playerLives)
     }
 } // End of Fifth cycle
 
 // Final game outcome message
-if (playerLives == 0 || dealerLives == 0) {
-    if (playerLives == 0) {
-        confirm("😢 Game Over! You died! Dealer wins! 💀")
-    } else {
-        confirm("😊 Congratulations! You killed the dealer! 🎉")
-    }
-} else {
+if (playerLives > 0 && dealerLives == 0) {
+    confirm("😊 Congratulations! You killed the dealer! 🎉")
+}
+
+if (playerLives == 0 && dealerLives > 0) {
+    confirm("😢 Game Over! You died! Dealer wins! 💀")
+}
+
+if (playerLives > 0 && dealerLives > 0) {
+    // defualt ending
     let continueGame = confirm("🕹️ All cycles are completed!\nDo you want to start a new session? (OK = Yes, Cancel = No)")
     if (continueGame) {
         // In this simple structure, instruct the user to refresh the page.
