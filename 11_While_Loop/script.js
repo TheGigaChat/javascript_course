dc56ceaf5eb6829d29108bd4c994807ccbcdeb56
Artur Tudeng <ardzek@taltech.ee>
2025-04-30 07:32:30 +0300

    While loop basic files

---
+++ 11_While_Loop/script.js
@@ -0,0 +1,42 @@
+// basic example
+// let number = 1
+// while (number < 10) {
+//     console.log(number)
+//     number = number + 1
+// }
+
+// let number2 = 1
+// console.log(number2)
+// number2 = number2 + 1
+// console.log(number2)
+// number2 = number2 + 1
+// console.log(number2)
+// number2 = number2 + 1
+// console.log(number2)
+// number2 = number2 + 1
+// console.log(number2)
+// number2 = number2 + 1
+// console.log(number2)
+// number2 = number2 + 1
+// console.log(number2)
+// number2 = number2 + 1
+// console.log(number2)
+// number2 = number2 + 1
+// console.log(number2)
+// number2 = number2 + 1
+
+// syntax
+
+// if (myMoodIsGood) {
+//     console.log("Welcome back!")
+// } else {
+//     console.log("Where are my potatoes??")
+// }
+
+// let myMoodIsGood = true
+// while (myMoodIsGood) {  // false
+//     console.log(myMoodIsGood)
+//     myMoodIsGood = false
+// }
+
+// console.log("Next section")
