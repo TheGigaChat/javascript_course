df142e95f036469a0e2f3bad226326186ff9fab5
Artur Tudeng <ardzek@taltech.ee>
2025-05-28 14:44:39 +0300

    15 Lesson Files

--- 15_consts_let_var/script.js
+++ 15_consts_let_var/script.js
@@ -15,24 +15,22 @@ favouriteCoffee = "Latte"
 console.log("My favourite coffee is: " + favouriteCoffee)
 
 
-// // consts create constants of data that can't be changed
-// const capitalCity = "London"
-// console.log(capitalCity + " is the capital of great britain.")
-
-// // after 5 years
-// console.log(capitalCity + " is the capital of great britain.")
-
-
-// comparison of let and const
-// 1000 lines
-
-let capitalCity = "London"
+// consts create constants of data that can't be changed
+const capitalCity = "London"
 console.log(capitalCity + " is the capital of great britain.")
 
 // after 5 years
-capitalCity = "Manchester"
 console.log(capitalCity + " is the capital of great britain.")
 
+
+// comparison of let and const
+let x = 10
+
+// 1000 lines
+x = 100
+console.log(x / 100)  // 1
+
+console.log(x * 10) // 100
 
 // rule what to choose
 let favouriteAnimal = "Cat"
