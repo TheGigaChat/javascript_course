2481e29b0cf08c43bb82123ac6eb72fceebe3deb
ArthurTudeng <ardzek@taltech.ee>
2025-06-04 14:10:25 +0300

    12 Lesson Files and Description

--- 12_While_Loop_Practice/challenge.js
+++ 12_While_Loop_Practice/challenge.js
@@ -74,21 +74,21 @@ console.log(letter)
 
 // while (message.length < 6) -> write a message
 // use a confirm() to ask the user's choice
-// ok -> add a chosen letter: default is "A"
+// ok -> add the chosen letter: default is "A"
 // cancel -> switch "A" to "B" and "B" to "A"
 
 let message = ""
 let currentLetter = "A"
 confirm("Please write a message.")
-while (message.length < 6) {
+while (message.length < 5) {
     let userChoice = confirm("Current letter is: " + currentLetter)
     if (userChoice) {
         message = message + currentLetter
         console.log(message)
     } else {
-        if (currentLetter == "A") {
+        if (currentLetter === "A") {
             currentLetter = "B"
-        } else if (currentLetter == "B") {
+        } else if (currentLetter === "B") {
             currentLetter = "A"
         }
     }
