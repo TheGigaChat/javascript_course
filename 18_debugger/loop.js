85d02675ca6ad82a5999dff210bf8d52534a19a8
ArthurTudeng <ardzek@taltech.ee>
2025-06-19 15:17:56 +0300

    18 Lesson Files

---
+++ 18_debugger/loop.js
@@ -0,0 +1,10 @@
+// First loop, sum all numbers from 1 to 50  // answer is 1275
+// Divide the sum by 75  // answer is 17
+
+let sum = 0
+for (let i = 1; i <= 50; i++) {
+    sum += i
+}
+sum = sum / 75
+
+console.log(sum)
