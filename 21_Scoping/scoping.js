4877124fe806f08d900415db05c5d55a47040d3b
ArthurTudeng <ardzek@taltech.ee>
2025-07-09 09:15:03 +0300

    21 Lesson Files Update

--- 21_Scoping/scoping.js
+++ 21_Scoping/scoping.js
@@ -8,6 +8,8 @@ if (myVariable === 1) {
     console.log(myVariable)
 }
 
+console.log(myVariable)
+
 for (let i = 0; i < 2; i++) {
     const myVariable = 3
     console.log(myVariable + i)
@@ -27,5 +29,47 @@ if (true) {
 if (true) {
     let x = 10
     x++
+    console.log(x)  // change only local variable
+}
+console.log(x)  // global is still the same
+
+if (true) {
+    x++
     console.log(x)
+}
+console.log(x)
+
+if (true) {
+    x = 1
+    console.log(x)
+}
+console.log(x)
+
+if (true) {
+    let y = 1
+    console.log(y)
+}
+// console.log(y)  // throws an error
+
+if (true) {
+    // console.log(y)  // throws an error
+}
+console.clear()
+
+
+// Nested Local Scopes
+if (true) {
+    let x = 10
+    console.log(x)
+    if (true) {
+        console.log(x)
+        let y = 1
+        console.log(y)
+    }
+    // console.log(y)  // throws an error
+}
+
+if (true) {
+    let x = 20
+    console.log(x)  // throws an error until we declare x in a local scope
 }
