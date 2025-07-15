980e05400ee9af7d492151062342cac49685ab78
ArthurTudeng <ardzek@taltech.ee>
2025-07-15 19:01:49 +0300

    22 Lesson Files

---
+++ 22_Functions_Basics/functions.js
@@ -0,0 +1,48 @@
+// Function - Is a reusable block of code.
+
+// function email() {
+//     console.log("Hi!")
+//     console.log("Something important...")
+//     console.log("All the best, Bob")
+// }
+
+// // send to friend1
+// email()
+
+// // send to friend2
+// email()
+
+
+// function emailParam(message) {
+//     console.log("Hi!")
+//     console.log(message)
+//     console.log("All the best, Bob")
+// }
+
+// // send to friend1
+// emailParam("My message")
+
+// // let message = "My message"  // explanations of arguments and parameters
+// // console.log(message)
+
+// // send to friend2
+// emailParam("How are you!")
+
+
+
+
+// function emailReturn(message) {
+//     console.log("Hi!")
+//     console.log(message)
+//     console.log("All the best, Bob")
+    
+//     // send back a responce that the message is arrived successfully
+//     return "Arrived"
+// }
+
+// const emailResponce = emailReturn("message")  // "Arrived"
+// console.log(emailResponce)
+
+// console.log(emailReturn("message"))
+
+// confirm(emailReturn("message"))  // advanced
