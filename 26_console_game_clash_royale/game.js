d40c02233ae3e6141e7d06957ba79f3e03bd4fe4
Arthur Tudeng <151076331+TheGigaChat@users.noreply.github.com>
2025-08-24 11:48:47 +0300

    26 Lesson Files

--- 26_console_game_clash_royale/game.js
+++ 26_console_game_clash_royale/game.js
@@ -46,7 +46,7 @@ function moveEnemies() {
         for (let j = 4; j >= 0; j--) {
             const tile = row[j]  // "#"
             if (tile === "E") {
-                const collision = enemyCollision()  // true, false, undefiend
+                const collision = enemyCollision(i, j)  // true, false, undefiend
 
                 if (collision === undefined) {
                     setWinner("E")  // stops the game and declares a winner
@@ -65,16 +65,81 @@ function moveEnemies() {
     }
 }
 
-function enemyCollision() {
+function enemyCollision(i, j) {
+    const nextRow = mapArray[i + 1]  // undefiend or ["#", "#", "#", "#", "#"]
+    if (nextRow === undefined) {
+        return undefined
+    }
+    const nextTile = nextRow[j]  // "#" or "A"
+    if (nextTile === "#") {
+        return false
+    }
+    if (nextTile === "A") {
+        return true
+    }
+}
 
+function moveAllies() {
+    for (let i = 0; i < 5; i++) {
+        const rowArray = mapArray[i]
+        for (let j = 0; j < 5; j++) {
+            const tile = rowArray[j]
+
+            if (tile === "A") {
+                const collision = allyCollision(i, j)  // true, false, undefiend
+
+                if (collision === undefined) {
+                    setWinner("A")
+                    return
+                } else if (!collision) {
+                    rowArray[j] = "#"
+                    const nextRow = mapArray[i - 1]
+                    nextRow[j] = "A"
+                } else {
+                    rowArray[j] = "#"
+                    const nextRow = mapArray[i - 1]
+                    nextRow[j] = "#"
+                }
+            }
+        }
+    }
+}
+
+function allyCollision(i, j) {
+    const nextRow = mapArray[i - 1]  // undefiend or ["#", "#", "#", "#", "#"]
+    if (nextRow === undefined) {
+        return undefined
+    }
+    const nextTile = nextRow[j]
+    if (nextTile === "#") {
+        return false
+    }
+    if (nextTile === "E") {
+        return true
+    }
 }
 
 function setWinner(_winner) {
-
+    winner = _winner
+    if (winner === "E") {
+        console.log("You lose...")
+    } else if (winner === "A") {
+        console.log("You win!")
+    }
 }
 
+drawMap()
 while (!winner) {
-    drawMap()
+    // Game
+    moveEnemies()
+    if (winner) {
+        break
+    }
+    moveAllies()
+    if (winner) {
+        break
+    }
+    addEnemy()
     addAlly()
-    break
+    drawMap()
 }
