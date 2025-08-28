1facb2d7a492429c41dfa72e122398c81e1d89d3
Arthur Tudeng <151076331+TheGigaChat@users.noreply.github.com>
2025-08-28 16:21:09 +0300

    27 Lesson Files

---
+++ 27_Math.methods()/script.js
@@ -0,0 +1,38 @@
+// Basics
+console.log(5 + 5)  // addition
+console.log(5 - 1)  // substruction
+console.log(10 * 2)  // multiplication
+console.log(4 / 2)  // division
+
+
+// Constans
+console.log(Math.PI)
+
+
+// Pow and Log
+console.log(Math.pow(10, 2))
+console.log(Math.pow(2, 3))
+console.log(Math.log2(32))
+
+
+// Max and Min
+console.log(Math.min())
+console.log(Math.min(1))
+console.log(Math.min(1, -1))
+
+const numbers = [1, 10, 2]
+console.log(Math.min(...numbers))  // NaN
+
+
+// Rounders
+console.log(Math.ceil(5.5))  // 6
+console.log(Math.round(5.5))  // 6
+console.log(Math.floor(5.5))  // 5
+console.log(Math.trunc(5.5))  // 5
+
+
+// Useful Math Methods
+console.log(Math.random())
+console.log(Math.abs(-2))
+console.log(Math.abs(2))
+console.log(Math.sign(1))
