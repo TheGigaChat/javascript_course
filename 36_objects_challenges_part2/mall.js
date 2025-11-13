f7d375d679d32d2ae7cbca6f29e26513212717e0
ArthurTudeng <artur.dzekunov10@gmail.com>
2025-11-13 14:04:08 +0200

    36 Lesson Files

---
+++ 36_objects_challenges_part2/mall.js
@@ -0,0 +1,101 @@
+// -------------------- Data --------------------
+const stuff = [
+  "water", "fire", "stone", "air", "earth", "fire",
+  "banana", "water", "dust", "air", "earth", "fire"
+]
+
+// -------------------- PART 1: Inventory --------------------
+const inventory = {
+  water: 0,
+  fire: 0,
+  air: 0,
+  earth: 0,
+
+  addElements(list) {
+    // TODO: increase counts for valid elements (ignoring invalid elements)
+    for (let i = 0; i < list.length; i++) {
+      const element = list[i]
+
+      if (element === "water") {
+        this.water++
+      }
+      if (element === "fire") {
+        this.fire++
+      }
+      if (element === "air") {
+        this.air++
+      }
+      if (element === "earth") {
+        this.earth++
+      }
+
+    }
+  },
+
+  addElement(element) {  // papaya
+    // TODO: increase the count of the corresponding element (ignoring invalid elements)
+    if (this[element] !== undefined) {
+      this[element]++
+    }
+  },
+
+  takeElement(element) {
+    // TODO: decrease the count of the corresponding element (ignoring invalid elements)
+    // If the element count is 0, display the message "You don't have this resource."
+    if (this[element] !== undefined) {
+      if (this[element] === 0) {
+        console.log("You don't have this resource")
+      } else {
+        this[element]--
+      }
+    }
+  },
+}
+
+console.log("== PART 1: Build inventory ==");
+inventory.addElements(stuff)
+console.log("Inventory after addElements:", inventory);
+// Inventory after addElements: {water: 2, fire: 3, air: 2, earth: 2, ...}
+
+inventory.addElement("water");
+inventory.addElement("water");
+inventory.addElement("fire");
+inventory.addElement("papaya"); // invalid => does nothing
+console.log("Inventory after addElement:", inventory);
+// Inventory after operations: {water: 4, fire: 4, air: 2, earth: 2}
+
+inventory.takeElement("fire");
+inventory.takeElement("banana"); // invalid => does nothing
+inventory.takeElement("air");
+inventory.takeElement("air");
+inventory.takeElement("air"); // hits zero => "You don't have this resource."
+console.log("Inventory after takeElement:", inventory);
+// Inventory after operations: {water: 4, fire: 3, air: 0, earth: 2}
+
+
+// // -------------------- PART 2: Alchemy Lab --------------------
+// const alchemyLab = {
+//   // 1 water, 2 air => speedPotion{speed: 50}
+//   // 2 fire, 1 earth => powerPotion{damage: 5}
+//   // 2 water, 1 earth => healthPotion{hp: 30}
+
+//   craftPotion(potionName, inventory) {
+//     // TODO: craft a potion using the recipes (ignoring invalid names)
+//     // when you craft a potion, you should take the recourses from the inventory.
+//     // if in the inventory is not enough recourses, display a message.
+//   },
+// }
+
+// console.log("\n== PART 2: Craft potions ==")
+// const p1 = alchemyLab.craftPotion("speedPotion", inventory)
+// // You don't have enough recourses.
+// const p2 = alchemyLab.craftPotion("powerPotion", inventory)
+// const p3 = alchemyLab.craftPotion("healthPotion", inventory)
+
+// console.log("Craft results:", p1, p2, p3)
+// // Craft results: {} {damage: 5} {hp: 30}
+
+// console.log("Inventory after crafting:", {
+//   water: inventory.water, fire: inventory.fire, air: inventory.air, earth: inventory.earth
+// })
+// // Inventory after crafting: {water: 1, fire: 0, air: 0, earth: 0}
