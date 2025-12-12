e13c3e25f58eda64c7cf048951d8cf0dc806d91e
ArthurTudeng <artur.dzekunov10@gmail.com>
2025-12-12 16:18:15 +0200

    38 Lesson Initial Files

---
+++ 39_Battle_Game/script.js
@@ -0,0 +1,23 @@
+class Unit {
+    name
+    hp
+    damage
+
+    constructor(name, hp, damage) {
+        this.name = name
+        this.hp = hp
+        this.damage = damage
+    }
+}
+
+// TODO 1: Create player and enemy objects using the Unit class
+
+// TODO 2: Get HTML elements (hp spans, button, log)
+
+// TODO 3: Implement battle logic function
+
+// TODO 4: Update UI function
+
+// TODO 5: Winning check function
+
+// TODO 6: Game runs after each attack
