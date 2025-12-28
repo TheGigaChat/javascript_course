fba30ae164edcc5b4fbf1bc08452a33fa81c357d
ArthurTudeng <artur.dzekunov10@gmail.com>
2025-12-28 19:35:08 +0200

    41 Lesson Files

---
+++ 41_template_literals/script.js
@@ -0,0 +1,13 @@
+// Template Literals
+
+const myName = "Bob"
+const myAge = 30
+
+console.log("My name is " + myName + " and my age is " + myAge + ".")
+console.log(`My name is ${myName} and my age is ${myAge}.`)
+
+console.log("My age is " + myAge + 1)
+console.log(`My age is ${myAge + 1}`)
+
+console.log("Quotes are: ")
+console.log(`Quotes are: ""`)
