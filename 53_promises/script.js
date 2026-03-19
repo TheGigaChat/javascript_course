96a826261a0afd48b6af38991638d8fe0eaa9246
ArthurTudeng <artur.dzekunov10@gmail.com>
2026-03-19 11:30:40 +0200

    53 Lesson Files

---
+++ 53_promises/script.js
@@ -0,0 +1,31 @@
+// Fetch, Promise, async/await
+
+// console.log("action 1")  // 1ms
+// console.log("action 2")  // 1ms
+// console.log("action 3")  // 1ms
+
+// Promise status: pending, fulfilled, rejected
+
+async function fetchUsers() {
+    const users = await fetch("https://jsonplaceholder.typicode.com/users")  // 200ms
+    const usersJson = await users.json()  // 100 ms
+    // console.log(usersJson)  // Response: ReadableStream => string => JSON (object)
+
+    for (let i = 0; i < usersJson.length; i++) {
+        const user = usersJson[i]
+        console.log(user)
+    }
+}
+
+fetchUsers()  // async
+
+console.log("action 1")  // 1ms
+console.log("action 2")  // 1ms
+console.log("action 3")  // 1ms
+
+// sync vs async
+
+
+// setTimeout(() => {
+//     console.log(users)  // Promise: fulfilled
+// }, 1000)
